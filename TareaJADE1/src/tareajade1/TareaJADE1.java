09bebbee73ec1c2be5fbcc63b9007edad52e75fe
Luis <Luis@localhost>
2017-05-10 16:06:23 -0500

    Argumentos para netbeans

--- TareaJADE1/src/tareajade1/TareaJADE1.java
+++ TareaJADE1/src/tareajade1/TareaJADE1.java
@@ -14,6 +14,7 @@ public class TareaJADE1 {
     /**
      * @param args the command line arguments
      */
+    //-agents "agent1:agents.SimpleAgent;agent2:agents.SimpleAgent;agent3:agents.SimpleAgent" -gui
     public static void main(String[] args) {
         // TODO code application logic here
     }
